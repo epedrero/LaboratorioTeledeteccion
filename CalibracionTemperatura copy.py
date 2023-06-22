37a630ad77de5d49a2b1132d1fad2a34c34d7c1f
unknown <e.pedrero01@ufromail.cl>
2023-06-22 11:00:15 -0400

    LTS

--- CalibracionTemperatura copy.py
+++ CalibracionTemperatura copy.py
@@ -1,8 +1,9 @@
 from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
 from datetime import date
 import pandas as pd
+import rasterio
 
-# connect to the API
+# Conectar a la API
 user = 'epedrero'
 password = 'Maricarmen2023!'
 api = SentinelAPI(user, password, 'https://scihub.copernicus.eu/dhus')
@@ -11,11 +12,22 @@ api = SentinelAPI(user, password, 'https://scihub.copernicus.eu/dhus')
 geojson_path = r"C:\Users\LABORATORIO\Documents\Ernesto\LaboratorioTeledeteccion\Region\AOI_test.geojson"
 footprint = geojson_to_wkt(read_geojson(geojson_path))
 
+# Consulta de productos
 products = api.query(footprint,
                      date=(date(2022, 10, 1), date(2023, 5, 14)),
-                     platformname='Sentinel-3',
-                     producttype='OL_1_EFR___')
+                     platformname='Sentinel-2',
+                     producttype='S2MSI1C' # Productos de Sentinel-2 multibanda
+                     )
 
-products_df = api.to_dataframe(products)
-path = r"C:\Users\LABORATORIO\Documents\Ernesto\LaboratorioTeledeteccion\productsimages.xlsx"
-products_df.to_excel(path, index=False)
+# Abrir el producto con rasterio
+with rasterio.open(products.path) as src:
+    # Obtener las bandas 11, 12 y 8
+    b11 = src['B11']
+    b12 = src['B12']
+    b8 = src['B8']
+
+
+'''
+# Descargar los productos encontrados
+api.download_all(products)
+'''
